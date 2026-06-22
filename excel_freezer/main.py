b0c99cee7ccf757359fe462503034b0916f37554
Ihor Sokolov <sihorjs@gmail.com>
2026-06-22 09:20:24 +0300

    simplify entry point

--- excel_freezer/main.py
+++ excel_freezer/main.py
@@ -4,29 +4,26 @@ from excel_freezer.services.dialog_manager import DialogManager
 from excel_freezer.services.excel_freezer import ExcelFreezer
 
 
-def freeze_excel_values():
-    dialog_manager = DialogManager()
-
-    input_path = dialog_manager.request_table_to_freeze()
-
-    if not input_path:
-        return
-
-    excel_freezer = ExcelFreezer(source=Path(input_path))
-
-    output_path = dialog_manager.request_path_for_save(
-        default_file_name=excel_freezer.default_output_file_name,
-    )
-
-    if not output_path:
-        return
-
+if __name__ == "__main__":
     try:
+        dialog_manager = DialogManager()
+        input_path = dialog_manager.request_table_to_freeze()
+
+        if not input_path:
+            raise Exception('No source file is selected')
+
+        source = Path(input_path)
+        excel_freezer = ExcelFreezer(source=source)
+
+        output_path = dialog_manager.request_path_for_save(
+            default_file_name=excel_freezer.default_output_file_name,
+        )
+
+        if not output_path:
+            raise Exception('No output path is selected')
+
         excel_freezer.run()
         dialog_manager.show_success_message(message=f"Готово!\nФайл збережено як:\n{output_path}")
     except Exception as error:
-        dialog_manager.show_error_message(error=error)
-
-
-if __name__ == "__main__":
-    freeze_excel_values()
+        if dialog_manager:
+            dialog_manager.show_error_message(error=error)
