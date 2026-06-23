5a77a87292ddbadff6631f85a3f68839b215849f
Ihor Sokolov <sihorjs@gmail.com>
2026-06-23 10:50:52 +0300

    reorganize flow

--- excel_freezer/main.py
+++ excel_freezer/main.py
@@ -1,28 +1,35 @@
 from pathlib import Path
 
+from excel_freezer.services.configuration import ConfigurationService
 from excel_freezer.services.dialog_manager import DialogManager
 from excel_freezer.services.excel_freezer import ExcelFreezer
 
-
 if __name__ == "__main__":
     try:
+        configuration_service = ConfigurationService()
         dialog_manager = DialogManager()
+
+        table_processing_paths = configuration_service.bootstrap()
         input_path = dialog_manager.request_table_to_freeze()
 
         if not input_path:
-            raise Exception('No source file is selected')
+            raise Exception("No table selected")
 
-        source = Path(input_path)
-        excel_freezer = ExcelFreezer(source=source)
-
-        output_path = dialog_manager.request_path_for_save(
-            default_file_name=excel_freezer.default_output_file_name,
-        )
+        table_path = Path(input_path)
+        default_output_file_name = ExcelFreezer.get_default_output_file_name(source=table_path)
+        output_path = dialog_manager.request_save_path(default_file_name=default_output_file_name)
 
         if not output_path:
-            raise Exception('No output path is selected')
+            raise Exception("No output path is selected")
 
+        freeze_table_configuration = configuration_service.get_freeze_table_configuration(
+            source=table_path,
+            destination=Path(output_path),
+        )
+
+        excel_freezer = ExcelFreezer(configuration=freeze_table_configuration)
         excel_freezer.run()
+
         dialog_manager.show_success_message(message=f"Готово!\nФайл збережено як:\n{output_path}")
     except Exception as error:
         if dialog_manager:
